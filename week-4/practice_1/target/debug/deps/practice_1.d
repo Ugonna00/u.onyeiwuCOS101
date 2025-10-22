d4d3ffbbe9e7cf287484846336ded342299b552c
Ugonna00 <ugonna.onyeiwu@pau.edu.ng>
2025-10-22 13:30:02 +0100

    (First practice)

---
+++ week-4/practice_1/target/debug/deps/practice_1.d
@@ -0,0 +1,5 @@
+C:\Users\HomePC\Documents\u.onyeiwuCOS101\week-4\practice_1\target\debug\deps\practice_1.d: src\main.rs
+
+C:\Users\HomePC\Documents\u.onyeiwuCOS101\week-4\practice_1\target\debug\deps\practice_1.exe: src\main.rs
+
+src\main.rs:
