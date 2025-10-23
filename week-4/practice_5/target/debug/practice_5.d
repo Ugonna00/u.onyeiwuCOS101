1e6cfdae0a1044563d92f38a4ba00850df151508
Ugonna00 <ugonna.onyeiwu@pau.edu.ng>
2025-10-23 13:25:31 +0100

    (Remaining Work)

---
+++ week-4/practice_5/target/debug/practice_5.d
@@ -0,0 +1,1 @@
+C:\Users\HomePC\Documents\u.onyeiwuCOS101\week-4\practice_5\target\debug\practice_5.exe: C:\Users\HomePC\Documents\u.onyeiwuCOS101\week-4\practice_5\src\main.rs
---
+++ week-4/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\HomePC\Documents\u.onyeiwuCOS101\week-4\practice_8\target\debug\practice_8.exe: C:\Users\HomePC\Documents\u.onyeiwuCOS101\week-4\practice_8\src\main.rs
